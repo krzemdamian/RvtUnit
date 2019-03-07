0d71f6d98239377d9ff5e194759887ec2ba7b13f
Damian Krzeminski <krzemdamian@gmail.com>
2019-03-07 18:29:05 +0100

    Change RvtUnit to work as applicaton instead ExternalCommand

---
+++ rvtUnit/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.UI;
+using System.IO;
+using System.Reflection;
+
+namespace rvtUnit
+{
+    public class EntryPoint : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = application.CreateRibbonPanel("RvtUnit");
+            PushButtonData data = new PushButtonData(
+                "RunTests", "Run Tests",
+                    Assembly.GetExecutingAssembly().Location,
+                    "rvtUnit.Commands.rvtUnitCmd");
+            PushButton button = panel.AddItem(data) as PushButton;
+
+            return Result.Succeeded;
+        }
+    }
+}
